389825bf4a96eb17d46f1cd30098a752517bcd73
Riston <ristonthomas@gmail.com>
2023-06-19 07:23:03 +0400

    Completed

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,13 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function(){
+   // console.log(links.classList);
+
+    links.classList.toggle('show-links');
+});
