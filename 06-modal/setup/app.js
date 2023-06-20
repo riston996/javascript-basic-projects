4a7d4fa9a337a67c01c5d8ef84c240f7c232bf9e
Riston <ristonthomas@gmail.com>
2023-06-21 03:48:07 +0400

    completed

--- 06-modal/setup/app.js
+++ 06-modal/setup/app.js
@@ -2,3 +2,17 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+const btn = document.querySelector(".btn.modal-btn");
+const modal = document.querySelector(".modal-overlay");
+const close = document.querySelector(".close-btn");
+
+btn.addEventListener("click",function() {
+    modal.classList.toggle("open-modal");
+
+});
+
+close.addEventListener("click",function() {
+    modal.classList.toggle("open-modal");
+
+});
