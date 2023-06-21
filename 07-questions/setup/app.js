dc8f9f25db5d6b948dea0cad138289d936cb7262
Riston <ristonthomas@gmail.com>
2023-06-21 04:40:13 +0400

    updated close other functions

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -7,6 +7,14 @@ questions.forEach(function(question) {
     const btn = question.querySelector('.question-btn');
     // console.log(btn);
     btn.addEventListener("click", function() {
+
+        questions.forEach(function (item) {
+            if (item !== question) {
+                item.classList.remove("show-text");
+            }
+        });
+
+
         question.classList.toggle("show-text");
     })
 
