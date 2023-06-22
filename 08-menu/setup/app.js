fc93192a0715e4ff159d237e30ca1b6c62b7c607
Riston <ristonthomas@gmail.com>
2023-06-22 07:02:00 +0400

    completed

--- 08-menu/setup/app.js
+++ 08-menu/setup/app.js
@@ -71,4 +71,86 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "steak",
+    category: "dinner",
+    price: 39.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
+
+const container = document.querySelector('.btn-container');
+const sectionCenter = document.querySelector('.section-center');
+
+
+
+//load items
+window.addEventListener("DOMContentLoaded", function() {
+  displayMenuItems(menu);
+  displayMenuButtons() ;
+
+}) ;
+
+function displayMenuButtons() {
+  const categories = menu.reduce(function(values, item){
+    if(!values.includes(item.category)){
+      values.push(item.category);
+    }
+    return values;
+    
+    }, 
+    ['all']);
+    const categoryBtns = categories.map(function(category){
+      return `<button class="filter-btn" type="button" data-id=${category}>${category}</button>`
+    }).join("");
+    container.innerHTML = categoryBtns;
+    const filterBtns = document.querySelectorAll('.filter-btn');  
+     //filter items
+  filterBtns.forEach(function(btn){
+    btn.addEventListener("click", function(e) {
+     const category = e.currentTarget.dataset.id ;
+     const menuCategory = menu.filter(function(menuItems){
+      console.log(menuItems.category);
+      if(menuItems.category === category){
+        return menuItems;
+      }
+      
+     });
+     if(category == "all") {
+      displayMenuItems(menu);
+     } else {
+      displayMenuItems(menuCategory);
+     }
+    });
+  });
+}
+
+
+ 
+
+
+
+function displayMenuItems(menuItems){
+  let displayMenu = menuItems.map(function (item) {
+    //console.log(item);
+    //console.log(item.id);
+     return `<article class="menu-item">
+     <img src=${item.img} class="photo" alt=${item.title}>
+     <div class="item-info">
+       <header>
+         <h4>${item.title}</h4>
+         <h4 class="price">${item.price}</h4>
+       </header>
+       <p>${item.desc}
+         
+       </p>
+       
+     </div>
+   </article>`;
+  });
+  displayMenu = displayMenu.join("");
+  sectionCenter.innerHTML = displayMenu;
+  
+};
